dd80e06b66797348c2258ffca00618245661c262
AlexeySilchenko <silchenko.a.a@gmail.com>
2024-02-15 02:49:15 +0300

    Отображение FullName в меню навигации

---
+++ ApplicationRent/Views/Shared/Components/Navigation/NavigationViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using ApplicationRent.Data.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApplicationRent.Views.Shared.Components.Navigation
+{
+    public class NavigationViewComponent : ViewComponent
+    {
+        private readonly UserManager<ApplicationIdentityUser> _userManager;
+
+        public NavigationViewComponent(UserManager<ApplicationIdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+            var fullName = user?.FullName ?? "Гость"; // Предполагая, что у вас есть свойство FullName
+            return View("Default", fullName);
+        }
+    }
+}
