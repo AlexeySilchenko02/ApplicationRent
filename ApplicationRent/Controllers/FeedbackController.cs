5b7a74cf4a11b38cd0a0d4114a390d7d0426b329
AlexeySilchenko <silchenko.a.e@gmail.com>
2024-03-19 19:32:24 +0300

    Add new page "Услуги" и "Обратная связь"

---
+++ ApplicationRent/Controllers/FeedbackController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApplicationRent.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
