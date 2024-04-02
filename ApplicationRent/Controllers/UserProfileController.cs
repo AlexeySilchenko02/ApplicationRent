d11e2b9de74361141c5dcb115fabd54bd7aaeeba
AlexeySilchenko <silchenko.a.e@gmail.com>
2024-04-02 16:01:44 +0300

    Отображение таблицы с текущими арендованными местами пользователя в ЛК

--- ApplicationRent/Controllers/UserProfileController.cs
+++ ApplicationRent/Controllers/UserProfileController.cs
@@ -1,16 +1,23 @@
-﻿using ApplicationRent.Data.Identity;
+﻿using ApplicationRent.App_data;
+using ApplicationRent.Data;
+using ApplicationRent.Data.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApplicationRent.Controllers
 {
     public class UserProfileController : Controller
     {
         private readonly UserManager<ApplicationIdentityUser> _userManager;
+        private readonly ApplicationDbContext _context;
+        private readonly FirebaseService _firebaseService;
 
-        public UserProfileController(UserManager<ApplicationIdentityUser> userManager)
+        public UserProfileController(UserManager<ApplicationIdentityUser> userManager, ApplicationDbContext context, FirebaseService firebaseService)
         {
             _userManager = userManager;
+            _context = context;
+            _firebaseService = firebaseService;
         }
 
         public async Task<IActionResult> Index()
@@ -18,10 +25,32 @@ namespace ApplicationRent.Controllers
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
-                return View("Error"); // Возвращайте представление ошибки или перенаправляйте, если пользователь не найден
+                return View("Error");
             }
 
-            return View(user);
+            var today = DateTime.Now;
+            var rentedPlaces = _context.Rentals
+                .Where(r => r.UserId == user.Id && r.EndRent > today)
+                .Select(r => new RentedPlaceViewModel // Используйте явно определенный класс вместо анонимного объекта
+                {
+                    Name = r.Place.Name,
+                    StartRent = r.StartRent,
+                    EndRent = r.EndRent
+                })
+                .ToList();
+
+            ViewBag.RentedPlaces = rentedPlaces;
+            ViewBag.FullNameUser = user.FullNameUser;
+
+            return View();
         }
+
+    }
+    public class RentedPlaceViewModel
+    {
+        public int RentalId { get; set; } // ID арендованного места
+        public string Name { get; set; }
+        public DateTime StartRent { get; set; }
+        public DateTime EndRent { get; set; }
     }
 }
