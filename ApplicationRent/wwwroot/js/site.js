55cd31be812f8efc91328d0fb5ab4b3339c0e234
huurrmma <hurmasort@gmail.com>
2024-05-02 19:11:44 +0300

    Отредактированные вход и регистрация, исправленная версия хедера с адаптивом

--- ApplicationRent/wwwroot/js/site.js
+++ ApplicationRent/wwwroot/js/site.js
@@ -1,4 +1,18 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+﻿const burger = document?.querySelector('[data-burger]');
+const nav = document?.querySelector('[data-nav]');
+const body = document.body;
+
+
+burger?.addEventListener('click', () => {
+    /*body.classList.toggle('stop-scroll');*/
+    burger?.classList.toggle('burger-active');
+    nav.classList.toggle('nav-visible');
+});
+
+
+
+
+// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
